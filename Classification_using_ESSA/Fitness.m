8d0395c807104fdcc06ee90879428f5e7e0c2c8f
Ayushmaan-Pandey <65823392+Ayushmaan-Pandey@users.noreply.github.com>
2020-05-23 23:33:51 +0530

    Update Fitness.m

--- Classification_using_ESSA/Fitness.m
+++ Classification_using_ESSA/Fitness.m
@@ -6,7 +6,7 @@ b = 50;
 r = (b-a).*rand(100,1) + a;
 r_r = [min(r)];
 z = r_r;
-SzW=0.01;
+p = 0.1;
  x=x>0.5;
  x=cat(2,x,zeros(size(x,1),1));
  x=logical(x);
@@ -18,4 +18,4 @@ end
 
 c = knnclassify(A(vald,x),A(trn,x),A(trn,end),30);
 cp = classperf(A(vald,end),c);
-y = z+(1-SzW)*(cp.CorrectRate)+SzW*sum(x)/(length(x)-1);
+y = z+(1-p)*(cp.CorrectRate)+p*sum(x)/(length(x)-1);
